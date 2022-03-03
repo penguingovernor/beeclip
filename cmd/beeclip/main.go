ea1d719fb1ce0a764264d18429eaf335dfa43291
Jorge Henriquez <contact@jorgehenriquez.dev>
2022-03-03 09:44:20 -0800

    Use env variables for wayland/xorg detection
    DISPLAY -> XORG
    WAYLAND_DISPLAY -> WAYLAND

--- cmd/beeclip/main.go
+++ cmd/beeclip/main.go
@@ -1,36 +1,25 @@
 package main
 
 import (
-	"io/ioutil"
 	"log"
 	"os"
-	"os/exec"
-	"runtime"
-	"strings"
+
+	"github.com/penguingovernor/beeclip/pkg/beeclip"
 )
 
 func main() {
-	// Grab the operating system.
-	osID := osString(runtime.GOOS)
-	if osID == unspecifiedLinux {
-		osID = whichLinux()
+	// See what operation to run.
+	binary := os.Args[0]
+	op := beeclip.CopyToClipboard
+	if binary == "beepaste" {
+		op = beeclip.PasteFromClipboard
 	}
 
-	// Then look up the appropriate command for
-	// the operating system.
-	commandMap := map[osString][]string{
-		windows:      {"clip.exe"},
-		darwin:       {"pbcopy"},
-		xorgLinux:    {"xclip", "-selection", "c"},
-		waylandLinux: {"wl-copy"},
+	// Get the command.
+	clipCmd, err := beeclip.WhichClipboardCommand(op)
+	if err != nil {
+		log.Fatalf("failed to get clipboard command: %s\n", err)
 	}
-	if _, ok := commandMap[osID]; !ok {
-		log.Fatalf("unsupported operating system: %s\n", osID)
-	}
-	cmd := commandMap[osID]
-
-	// Then build the command.
-	clipCmd := exec.Command(cmd[0], cmd[1:]...)
 	clipCmd.Stdin = os.Stdin
 	clipCmd.Stderr = os.Stderr
 	clipCmd.Stdout = os.Stdout
@@ -39,46 +28,4 @@ func main() {
 	if err := clipCmd.Run(); err != nil {
 		log.Fatalf("failed to paste to clipboard: %s\n", err)
 	}
-}
-
-type osString string
-
-const (
-	windows          osString = "windows"
-	darwin           osString = "darwin"
-	xorgLinux        osString = "xLinux"
-	waylandLinux     osString = "wLinux"
-	unspecifiedLinux osString = "linux"
-)
-
-// whichLinux assumes that runtime.GOOS is linux.
-// It will then return the proper osString corresponding to it.
-// If none of the tests work, then runtime.GOOS is returned.
-func whichLinux() osString {
-	// Check for WSL.
-	if isWSL() {
-		return windows
-	}
-	// Check for X11 or Wayland.
-	//
-	// Ref: https://unix.stackexchange.com/questions/202891/how-to-know-whether-wayland-or-x11-is-being-used
-	switch os.Getenv("XDG_SESSION_TYPE") {
-	case "x11":
-		return xorgLinux
-	case "wayland":
-		return waylandLinux
-	default:
-		return osString(runtime.GOOS)
-	}
-}
-
-// isWSL determines if the the caller is running under the Windows Subsystem for Linux.
-//
-// Ref: https://github.com/microsoft/WSL/issues/423#issuecomment-221627364
-func isWSL() bool {
-	kernelInfo, err := ioutil.ReadFile("/proc/sys/kernel/osrelease")
-	if err != nil {
-		return false
-	}
-	return strings.Contains(string(kernelInfo), "microsoft")
 }
--- pkg/beeclip/beeclip.go
+++ pkg/beeclip/beeclip.go
@@ -1,7 +1,106 @@
 package beeclip
 
-import "os/exec"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+)
 
-func WhichClipboardCommand() (*exec.Cmd, error) {
-	return nil, nil
+type ClipboardOperation uint
+
+const (
+	CopyToClipboard = ClipboardOperation(iota)
+	PasteFromClipboard
+)
+
+type clipboardCommands struct {
+	copyCommand  string
+	pasteCommand string
+}
+
+func WhichClipboardCommand(op ClipboardOperation) (*exec.Cmd, error) {
+	commands, ok := operatingSystemToClipboardCommands[determineOperatingSystemID(runtime.GOOS)]
+	if !ok {
+		return nil, fmt.Errorf("could not determine operating system")
+	}
+
+	commandInPath := func(cmd string) (*exec.Cmd, error) {
+		fields := strings.Fields(cmd)
+		if _, err := exec.LookPath(fields[0]); err != nil {
+			return nil, err
+		}
+		return exec.Command(fields[0], fields[1:]...), nil
+	}
+
+	switch op {
+	case CopyToClipboard:
+		return commandInPath(commands.copyCommand)
+	case PasteFromClipboard:
+		return commandInPath(commands.pasteCommand)
+	}
+
+	return nil, fmt.Errorf("unknown operation: %d", op)
+}
+
+type operatingSystemID uint
+
+const (
+	operatingSystemWindows = operatingSystemID(iota)
+	operatingSystemDarwin
+	operatingSystemXorgLinux
+	operatingSystemWaylandLinux
+	operatingSystemUnknown
+)
+
+var operatingSystemToClipboardCommands = map[operatingSystemID]clipboardCommands{
+	operatingSystemWindows: {
+		copyCommand:  "powershell -command Get-Clipboard",
+		pasteCommand: "powershell -command Set-Clipboard",
+	},
+	operatingSystemXorgLinux: {
+		copyCommand:  "xclip -selection c",
+		pasteCommand: "xclip -selection c -o",
+	},
+	operatingSystemWaylandLinux: {
+		copyCommand:  "wl-copy",
+		pasteCommand: "wl-paste",
+	},
+}
+
+func determineOperatingSystemID(operatingSystem string) operatingSystemID {
+	isWSL := func() bool {
+		kernelInfo, err := os.ReadFile("/proc/sys/kernel/osrelease")
+		if err != nil {
+			return false
+		}
+		return strings.Contains(string(kernelInfo), "microsoft")
+	}
+
+	whichLinux := func() operatingSystemID {
+		if isWSL() {
+			return operatingSystemWindows
+		}
+		// Check for wayland display.
+		if _, isWayland := os.LookupEnv("WAYLAND_DISPLAY"); isWayland {
+			return operatingSystemWaylandLinux
+		}
+		// Check for xorg display.
+		if _, isXorg := os.LookupEnv("DISPLAY"); isXorg {
+			return operatingSystemWaylandLinux
+		}
+		return operatingSystemUnknown
+	}
+
+	switch operatingSystem {
+	case "windows":
+		return operatingSystemWindows
+	case "darwin":
+		return operatingSystemDarwin
+	case "linux":
+		return whichLinux()
+	default:
+		return operatingSystemUnknown
+	}
 }
